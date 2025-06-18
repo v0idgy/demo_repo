49ef9cf5f6ca9f65ff8547d5dbe90515c45b8315
Gourav Yadav <v0idgy@gmail.com>
2025-06-19 00:59:22 +0530

    Second commit

--- demo_repo/Scripts/activate.bat
+++ demo_repo/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python_Projects\LPU\demo_repo\demo_repo
+set "VIRTUAL_ENV=D:\Python_Projects\LPU\demo_repo\demo_repo"
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -24,8 +24,8 @@ set PYTHONHOME=
 if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
-set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(demo_repo) 
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+set "VIRTUAL_ENV_PROMPT=(demo_repo) "
 
 :END
 if defined _OLD_CODEPAGE (
